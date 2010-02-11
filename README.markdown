1bfbb93eadc91218c8d65b41d20c926d6de4f6df
Joël Perras <joel.perras@gmail.com>
2010-02-11 13:03:14 -0500

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,48 @@
+SSL Component
+=============
+
+This Secured component allows you to programmatically define which controller actions
+should be served under a secure HTTPS connection.
+
+Most of the time, this functionality is acheived through judicous use of rewrite/redirect
+rules in your webserver (Apache, Lighhtpd, Nginx, etc.). Defining this logic in your webserver
+is advantageous - an incorrect request never hits your application code, and it could be handled
+by a proxy to ensure that your application servers are not bothered with requests they cannot serve.
+
+However, there are cases where the programmatic definition of which controllers & actions
+is desirable - 1) during development, 2) sitations where you do not have access to `.htaccess`
+or the webserver configuration, 3) when static definitions of secured URLs do not suffice.
+
+This very simple component attempts to address the above issues, and allows for a very intuitive
+and straightforward configuration. Here is a sample config, where we desire that the `login` action
+within the `users` controller to be served via HTTPS, and _all_ `store` actions to be served via HTTPS:
+
+
+    /* app_controller.php */
+
+    /**
+     * Components for all controllers.
+     *
+     * @var array Components, with optional configuration directives.
+     */
+    public $components = array(
+    	'Secured.Ssl' => array(
+    		'secured' => array(
+    			'users' => 'login',
+    			'store' => '*'
+    		)
+    	)
+    );
+
+Requirements:
+-------------
+ - A valid and properly installed/configured SSL certificate.
+ - This component.
+ - CakePHP 1.2/1.3 (the latter has not been tested, but should work without issue).
+
+Things that need to be done:
+----------------------------
+ - Test cases
+ - Implement `$autoRedirect` attribute to replace Configure::read('debug') check.
+ - Add more configurability for specifying the protocol (defaults to http/https) as well
+   as the subdomain (defaults to 'www' for secured connections).
---
+++ controllers/components/ssl.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * SSL Secure Component: Programmatically securing your controller actions.
+ *
+ * @copyright     Copyright 2010, Plank Design (http://plankdesign.com)
+ * @license       http://opensource.org/licenses/mit-license.php The MIT License
+ */
+
+/**
+ * SSL Component
+ *
+ * This SSL component allows you to programmatically define which controller actions
+ * should be served only under a secure HTTPS connection.
+ *
+ * Most of the time, this functionality is acheived through judicous use of rewrite/redirect
+ * rules in your webserver (Apache, Lighhtpd, Nginx, etc.). Defining this logic in your webserver
+ * is advantageous - an incorrect request never hits your application code, and it could be handled
+ * by a proxy to ensure that your application servers are not bothered with requests they cannot serve.
+ *
+ * However, there are cases where the programmatic definition of which controllers & actions
+ * is desirable - 1) during development, 2) sitations where you do not have access to .htaccess
+ * or the webserver configuration, 3) when static definitions of secured URLs do not suffice.
+ *
+ * This very simple component attempts to address the above issues. See the README for a sample
+ * configuration.
+ *
+ * @todo Implement $autoRedirect attribute to replace Configure::read('debug') check.
+ * @todo Test cases
+ */
+
+class SslComponent extends Object {
+
+	/**
+	 * Associative array of controllers & actions that need
+	 * to be served from HTTPS instead of regular HTTP.
+	 *
+	 * @var array
+	 */
+	public $secured = array();
+
+	/**
+	 * If the current request comes through SSL,
+	 * this variable is set to true.
+	 *
+	 * @var boolean True if request was made through SSL, false otherwise.
+	 */
+	public $https = false;
+
+	/**
+	 * Component initialize method.
+	 * Is called before the controller beforeFilter method. All local component initialization
+	 * is done here.
+	 *
+	 * @param object $controller A reference to the controller which initialized this component.
+	 * @param array $settings Optional component configurations.
+	 * @return void
+	 * @todo Perhaps move logic to startup() to allow more fine-grained programmatic control.
+	 * @todo Change Configure::read('debug') check to a $this->autoRedirect check.
+	 */
+	public function initialize(&$controller, $settings = array()) {
+		$this->controller = $controller;
+		$this->_set($settings);
+
+		if (env('HTTPS') === 'on' || env('HTTPS') === true) {
+			$this->https = true;
+		}
+
+		if (Configure::read('debug') == 0) {
+			$secured = $this->ssled($this->controller->params);
+
+			if ($secured && !$this->https) {
+				$this->forceSSL();
+			}
+			elseif (!$secured && $this->https) {
+				$this->forceNoSSL();
+			}
+		}
+
+	}
+
+	/**
+	 * Determines whether the request (based on passed params)
+	 * should be ssl'ed or not.
+	 *
+	 * @param array $params Parameters containing 'controller' and 'action'
+	 * @return boolean True if request should be ssl'ed, false otherwise.
+	 */
+	public function ssled($params) {
+		if (!array_key_exists($params['controller'], $this->secured)) {
+			return false;
+		}
+		$actions = (array) $this->secured[$params['controller']];
+
+		if ($actions === array('*')) {
+			return true;
+		}
+		return (in_array($params['action'], $actions));
+	}
+
+	/**
+	 * Redirects current request to be SSL secured
+	 *
+	 * @return void
+	 * @todo Make protocol & subdomain ('https' & 'www' configurable)
+	 * @todo allow conditional passing of server identifier
+	 */
+	public function forceSSL() {
+		$server = env('SERVER_NAME');
+
+		if (substr($server, 0, 4) === 'www.') {
+			$server = substr($server, 4);
+		}
+		$this->controller->redirect("https://www.$server{$this->controller->here}");
+	}
+
+	/**
+	 * Symmetric method to forceSSL, which redirects the current
+	 * executing request to non-SSL.
+	 *
+	 * @return void
+	 * @todo Make protocol & subdomain ('https' & 'www' configurable)
+	 * @todo allow conditional passing of server identifier
+	 */
+	public function forceNoSSL() {
+		$server = env('SERVER_NAME');
+
+		if (substr($server, 0, 4) === 'www.') {
+			$server = substr($server, 4);
+		}
+		$this->controller->redirect("http://www.$server{$this->controller->here}");
+	}
+
+}
+?>
